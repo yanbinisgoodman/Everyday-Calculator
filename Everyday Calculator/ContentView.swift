a7409ce56bd4bfd2aeaa043bd6158b73be8c0dd5
TechGuyBrad <techguybrad@gmail.com>
2021-05-31 12:53:40 -0400

    Initial Commit

---
+++ Everyday Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Everyday Calculator
+//
+//  Created by Brad Schneider on 5/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Everyday Calculator/Everyday_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Everyday_CalculatorApp.swift
+//  Everyday Calculator
+//
+//  Created by Brad Schneider on 5/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Everyday_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
